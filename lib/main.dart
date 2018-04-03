1ba5bdba91ebc718d787afe278540cf17471d525
Abhishek Bharadwaj <abhishek.bharadwaj@healthifyme.com>
2018-04-03 12:55:16 +0530

    Done with tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -35,9 +35,42 @@ class SampleWidget extends StatelessWidget {
 class DataWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
     return new Center(
-      child: new Text(wordPair.asPascalCase),
+      child: new RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordState();
+}
+
+class RandomWordState extends State {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildSuggestions();
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair suggestion) {
+    return new ListTile(
+      title: new Text(suggestion.asPascalCase, style: _biggerFont),
     );
   }
 }
